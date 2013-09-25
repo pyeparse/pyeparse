3befe0e1ace0300cc80e2d4fc6b999bf64514aae
dengemann <d.engemann@fz-juelich.de>
2013-09-25 22:31:49 +0200

    ENH: add temporary test file

--- test_parser.py
+++ test_parser.py
@@ -13,14 +13,5 @@ blink = 'EBLINK R 5495553    5495755 203'
 print parsextended.parse_blink(blink)
 fixation = 'EFIX R   5548321.0  5552261.0   3941      836.7   484.1     622   45.60   46.10'
 print parsextended.parse_fixation(fixation)
-message = 'MSG 5399829.0 trial-1_start_inc-soc-m_block-15_correct-l-time-2546256'
-print parsextended.parse_message(message)
-resp = '5551823.0 trial-11_ responses:1 button:1 type:response_other code:1 time:2697320 unc_dms:1'
-print parsextended.parse_button_press(resp)
 modality = 'MSG 5565983.0 Modality-hand-END'
 print parsextended.parse_modality(modality)
-
-# XXX add test file
-fname, n_trials = 'exp2/data/OSSRC_108411.asc', 192
-
-parsextended.data_frame_from_log(fname, n_trials)
