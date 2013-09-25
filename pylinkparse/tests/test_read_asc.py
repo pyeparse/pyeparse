8185a2eeae7a533591740e7f0202447c4574547c
dengemann <d.engemann@fz-juelich.de>
2013-09-25 23:16:09 +0200

    ENH: add pandas variant to test

--- pylinkparse/tests/test_read_asc.py
+++ pylinkparse/tests/test_read_asc.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 path = 'pylinkparse/tests/data/'
 fname = path + 'test_raw.asc'
@@ -20,3 +21,10 @@ saccades = np.genfromtxt(saccades, dtype=(['S8'] * 2) + (['f8'] * 11))
 blinks = np.genfromtxt(blinks, dtype=(['S8'] * 2) + (['f8'] * 3))
 
 fixations = np.genfromtxt(fixations, dtype=(['S8'] * 2) + (['f8'] * 8))
+
+df = pd.DataFrame(blinks)
+
+print df
+
+print df['f0'].dtype
+print df['f3'].dtype
