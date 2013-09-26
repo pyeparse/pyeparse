f7e5862f2931b301ffae662d923c631238fdcb6a
dengemann <d.engemann@fz-juelich.de>
2013-09-26 10:31:47 +0200

    ENH: add constants

---
+++ pylinkparse/constants.py
@@ -0,0 +1,22 @@
+# Authors: Denis Engemann <d.engemann@fz-juelich.de>
+#
+# License: BSD (3-clause)
+
+
+class Bunch(dict):
+    """ Dict that exposes keys as attributes """
+    def __init__(self, *args, **kwargs):
+        dict.__init__(self, *args, **kwargs)
+        self.__dict__ = self
+
+
+EDF = Bunch()
+
+EDF.CODE_SAC = 'ESACC'
+EDF.CODE_FIX = 'EFIX'
+EDF.CODE_BLINK = 'EBLINK'
+
+EDF.FIX = 'eye stime etime dur axp ayp aps'
+EDF.SAC = 'eye stime etime dur sxp syp exp eyp ampl'
+EDF.BLINK = 'eye stime etime dur'
+EDF.SAMPLE = 'time xpos posy ps xvEDF yvEDF xref yref'
