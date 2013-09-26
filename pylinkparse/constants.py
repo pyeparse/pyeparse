7bf16964c35be38fc4eb7f782f1091cd177cd535
dengemann <d.engemann@fz-juelich.de>
2013-09-26 18:21:17 +0200

    ENH: improve tests

--- pylinkparse/constants.py
+++ pylinkparse/constants.py
@@ -15,8 +15,14 @@ EDF = Bunch()
 EDF.CODE_SAC = 'ESACC'
 EDF.CODE_FIX = 'EFIX'
 EDF.CODE_BLINK = 'EBLINK'
-
-EDF.FIX = 'event eye stime etime dur axp ayp aps'
+EDF.FIX = 'event eye stime etime dur sxp syp ps resx resy'
 EDF.SAC = 'event eye stime etime dur sxp syp exp eyp ampl pvl resx resy'
 EDF.BLINK = 'event eye stime etime dur'
 EDF.SAMPLE = 'time xpos ypos ps xvEDF yvEDF xref yref N1 N2'
+EDF.BLINK_DTYPES = 'O O int64 int64 int64'
+
+EDF.FIX_DTYPES = ('O O float64 float64 int64 float64 float64 int64 '
+                  'float64 float64')
+
+EDF.SAC_DTYPES = ('O O float64 float64 int64 float64 float64 '
+                  'float64 float64 float64 int64 float64 float64')
--- pylinkparse/tests/test_read_asc.py
+++ pylinkparse/tests/test_read_asc.py
@@ -1,11 +1,10 @@
 import numpy as np
 from numpy.testing import assert_equal
-import pandas as pd
 from pylinkparse import Raw
+from pylinkparse.constants import EDF
 
 path = 'pylinkparse/tests/data/'
 fname = path + 'test_raw.asc'
-
 
 
 def test_raw_io():
@@ -18,15 +17,14 @@ def test_raw_io():
     assert_equal(len(dtypes), 1)
     assert_equal(dtypes[0], np.float64)
 
-    dtypes = raw._saccades.dtypes.unique()
-    assert_equal(len(dtypes), 3)
-    dtypes = raw._saccades.dtypes.values
-    assert_equal(raw._saccades['dur'], np.int64)
-    assert_equal(raw._saccades['pvl'], np.int64)
-    float_fields = 'stime etime sxp syp exp eyp ampl resx resy'.split()
-    dtypes = np.qunique(raw._saccades[float_fields])
-    assert_equal(len(dtypes), 1)
-    assert_equal(dtypes[0], np.int64)
+    for actual, desired in zip(raw._saccades.dtypes, EDF.SAC_DTYPES.split()):
+        assert_equal(actual, np.dtype(desired))
+
+    for actual, desired in zip(raw._blinks.dtypes, EDF.BLINK_DTYPES.split()):
+        assert_equal(actual, np.dtype(desired))
+
+    for actual, desired in zip(raw._fixations.dtypes, EDF.FIX_DTYPES.split()):
+        assert_equal(actual, np.dtype(desired))
 
 
 def tets_access_data():
