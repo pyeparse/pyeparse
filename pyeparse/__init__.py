aa964871864811fbff0fdd7552be6ef3373fbe7c
Eric Larson <larson.eric.d@gmail.com>
2014-03-18 15:52:22 -0700

    ENH: Remove unused file

--- pyeparse/__init__.py
+++ pyeparse/__init__.py
@@ -6,8 +6,6 @@ from . import utils  # noqa
 from .edf._raw import RawEDF  # noqa
 Raw = RawEDF  # noqa
 from .epochs import Epochs  # noqa
-from . import constants  # noqa
 from . import viz  # noqa
-from . import edf  # noqa
 
 __version__ = 0.01
